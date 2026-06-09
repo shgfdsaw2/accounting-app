e2073f54b1d9f3fa09d7cf8dd02c6e2a268e6f98
shgfdsaw2 <jabar66123@gmail.com>
2026-06-10 02:31:37 +0300

    Delete find_listeners.js

--- find_listeners.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('app.js', 'utf8');
-const lines = content.split('\n');
-lines.forEach((line, idx) => {
-  if (line.includes('.addEventListener')) {
-    console.log(`${idx + 1}: ${line.trim()}`);
-  }
-});
