c609a6ae9e055fd70501d4cea0d1ac6e2e816496
shgfdsaw2 <jabar66123@gmail.com>
2026-06-10 02:32:41 +0300

    Delete search_load.js

--- search_load.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('app.js', 'utf8');
-const match = content.match(/const loadInitialData =[\s\S]*?\n\};/);
-if (match) {
-  console.log(match[0]);
-} else {
-  console.log('loadInitialData not found!');
-}
