88f68daa01d786f5ffd2f2e2436072932c4178e7
shgfdsaw2 <jabar66123@gmail.com>
2026-06-10 02:32:28 +0300

    Delete search_global.js

--- search_global.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('app.js', 'utf8');
-const lines = content.split('\n');
-lines.forEach((line, idx) => {
-  if (/globalSearchBar|global-search-bar/i.test(line)) {
-    console.log(`${idx + 1}: ${line.trim()}`);
-  }
-});
-console.log('Search completed.');
