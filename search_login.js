2a46e5c1b4d775b575db6fc7f2c02d10a940b12b
shgfdsaw2 <jabar66123@gmail.com>
2026-06-10 02:32:52 +0300

    Delete search_login.js

--- search_login.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('app.js', 'utf8');
-const match = content.match(/const handleLogin =[\s\S]*?\n\};/);
-if (match) {
-  console.log(match[0]);
-} else {
-  console.log('handleLogin not found!');
-}
