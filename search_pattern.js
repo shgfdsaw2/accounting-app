7f358eb4e323c2b87616be00714ccc27235ce1b1
shgfdsaw2 <jabar66123@gmail.com>
2026-06-30 21:59:02 +0300

    Delete search_pattern.js

--- search_pattern.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('app.js', 'utf8');
-const lines = content.split('\n');
-
-lines.forEach((line, index) => {
-  if (line.includes('const selectCustomerInDropdown') || line.includes('function selectCustomerInDropdown')) {
-    console.log(`${index + 1}: ${line.trim()}`);
-  }
-});
